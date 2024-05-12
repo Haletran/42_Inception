38382a6426ca7ffa76dd6c20e87efab618b50f1d
Haletran <bpasquier123@gmail.com>
2024-05-12 16:09:47 +0200

    add preparation for tls443, fix user database wp, better header makefile

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -5,6 +5,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=baptiste --admin_password=1811 --admin_email=bapasqui@student.42angouleme.fr --allow-root
+./wp-cli.phar user create Michel Michel@gmail.com --role=editor --user_pass=815michmich --allow-root
+./wp-cli.phar user delete admin --allow-root
 
 php-fpm8.2 -F
