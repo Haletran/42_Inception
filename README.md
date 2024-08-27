9a1d53f75d43a94d59c8cd7d7e76cd18ce1c661f
Haletran <bpasquier123@gmail.com>
2024-08-27 18:10:49 +0200

    feat: add setup script for vm and change README

--- README.md
+++ README.md
@@ -4,8 +4,3 @@ This project aims to broaden your knowledge of system administration by using Do
 You will virtualize several Docker images, creating them in your new personal virtual
 machine.
 
-
-> Remove sudo only access to make  :
-```
-sudo usermod -aG docker $USER
-```
---
+++ scripts/setup_vm.sh
@@ -0,0 +1,67 @@
+#!bin/sh
+
+username=bapasqui
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root. (Using this command : su - or sudo the file)"
+  exit 1
+fi
+
+## UPDATE
+echo "Updating system"
+if [ -f /etc/arch-release ]; then
+    pacman -Syu
+    pacman -S ssh samba
+    install_yay
+    docker_setup
+    setup_smb
+else if [ -f /etc/debian_version ]; then
+    apt-get update
+    apt-get upgrade
+    apt-get install ssh samba
+    docker_setup
+    setup_smb
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
+## INSTALL YAY
+install_yay()
+{
+    echo "Installing yay"
+    pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+}
+
+docker_setup()-
+{
+    echo "Setup Docker"
+    ## INSTALL DOCKER
+    if [ -f /etc/arch-release ]; then
+        pacman -S docker docker-compose
+    else if [ -f /etc/debian_version ]; then
+        apt-get install docker docker-compose
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
+    usermod -aG docker $USER
+    ## install PORTAINER
+    docker volume create portainer_data
+    docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.0
+}
+
+setup_smb()
+{
+    echo "Setup Samba"
+    ## SETUP SAMBA share
+    mkdir inception
+    cp /etc/samba/smb.conf.default /etc/samba/smb.conf
+    sudo systemctl enabled smbd.socket
+    sudo systemctl start smbd.socket
+    useradd $username
+    echo -e $password"\n"$password | smbpasswd -s $username
+}
