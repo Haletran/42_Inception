5e723805051d0a6befa8934a943ef9d1aea289c3
Haletran <bpasquier123@gmail.com>
2024-09-19 15:40:52 +0200

    fix: mariadb working properly

--- srcs/requirements/mandatory/mariadb/tools/setup.sh
+++ srcs/requirements/mandatory/mariadb/tools/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 service mariadb start
+sleep 5
+# need to put this in a .env
 SQL_DATABASE="wordpress"
-SQL_USER="wordpress"
+SQL_USER="bapasqui"
 SQL_PASSWORD="1234"
 SQL_ROOT_PASSWORD="12345"
 
@@ -10,6 +12,7 @@ SQL_ROOT_PASSWORD="12345"
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
 CREATE USER IF NOT EXISTS \`$SQL_USER\`@'localhost' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'localhost';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
