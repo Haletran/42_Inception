a2b48dadf201fe0d6254b5d7908afb8eb3daae10
Haletran <bpasquier123@gmail.com>
2024-09-23 15:46:25 +0200

    trying to fix some stuff

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -3,13 +3,14 @@
 FTP_USER=ftp
 FTP_PASS=1234
 
-mkdir -p /var/www/html/
+mkdir -p /var/www/wordpress
 if ! id -u ftp &>/dev/null; then
     adduser ftp --disabled-password
 fi
 
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 chown -R $FTP_USER:$FTP_USER /var/www/wordpress
+chmod -R 755 /var/www/wordpress
 echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 
 vsftpd /etc/vsftpd/vsftpd.conf
